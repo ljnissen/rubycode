f1e59e5c11bbcae411eb45cc3babc460c53d0545
LJ Nissen <lj.nissen@gmail.com>
2017-07-14 12:49:07 +0200

    Assigning local variable as opposed to an explicit receiver

---
+++ 001/file_01.rb
@@ -0,0 +1,18 @@
+class Foo
+
+  def foo=(something)
+    puts "Now it's a #{something}!"
+  end
+
+  def do_foo_assign
+   
+    self.foo = "bar"
+  end
+end
+
+f = Foo.new
+f.do_foo_assign
+
+
+
+ # foo = "bar" 
