968180565f79f26aa7372b3b25d4c2257308051f
LJ Nissen <lj.nissen@gmail.com>
2017-07-15 10:15:53 +0200

    Adding operator-style method

--- 001/file_02.rb
+++ 001/file_02.rb
@@ -1,17 +1,16 @@
 class Foo
 
   def foo=(something)
-    puts "Now it's a #{something}!"
+    puts "It's a #{something}"
   end
 
-  def do_foo_assign
+  def foo_assign
     foo = "bar"
-    #self.foo = "bar"
   end
 end
 
 f = Foo.new
-f.do_foo_assign
+f.foo_assign
 
 
 
---
+++ 002/file_01.rb
@@ -0,0 +1,8 @@
+class Add10
+	def +(number)
+		10 + number
+	end
+end
+
+p(Add10.new + 5)
+
