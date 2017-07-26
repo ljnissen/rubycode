a47960e31eaad679bf7166114374268c6de0b463
LJ Nissen <lj.nissen@gmail.com>
2017-07-26 14:02:51 +0200

    Fooling around with #inject ii

--- 006/file_08.rb
+++ 006/file_08.rb
@@ -1,4 +1,4 @@
-n = [1, 2, 3, 4].inject(0) { |result, element| result + element } # 1 + 2 = 3; 3 + 3 = 6; 6 + 4 = 10
+n = [1, 2, 3, 4].inject(5) { |my_result, my_element| my_result + my_element } # 1 + 2 = 3; 3 + 3 = 6; 6 + 4 = 10; 10 + 5 = 15
 
 puts n
 
