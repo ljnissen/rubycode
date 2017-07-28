51e5db88387c42d27c401d7f0730ab7f539f72e3
LJ Nissen <lj.nissen@gmail.com>
2017-07-28 11:30:02 +0200

    Add comments to function file_12.rb

--- 006/file_12.rb
+++ 006/file_12.rb
@@ -1,10 +1,10 @@
 print "Please enter an integer: "
-def integer(n)
-	total = 1
-	(1..n).each do |n|
-		total = total * n
+def integer(n) # Defining method that takes integer n
+	total = 1 # Set total to 1
+	(1..n).each do |n| # Count up to n
+		total = total * n  # Multiply current value of total with current value of n
 	end
-total	
+total	# Return total
 end
 
-print "The factorial is: ",integer(gets.to_i)
+print "The factorial is: ",integer(gets.to_i) # Print factorial while absorbing an integer n
