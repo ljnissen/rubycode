f28f3bd9e08815a24a24fc174bffa4c875c02cf3
LJ Nissen <lj.nissen@gmail.com>
2017-07-29 12:08:39 +0200

    Baby steps towards understanding search

---
+++ 007/file_01.rb
@@ -0,0 +1,5 @@
+print "Please enter a number: "
+	def number(n)
+	end
+print "The number, " m "is contained within the first 5 numbers."
+print "The number, " m "does not appear within the first 5 numbers."
