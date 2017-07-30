30f1173b263763832a6520e0da3d52f1cd9ec82d
LJ Nissen <lj.nissen@gmail.com>
2017-07-30 13:17:54 +0200

    Understanding include(3) in array

--- 007/file_01.rb
+++ 007/file_01.rb
@@ -1,5 +1,11 @@
+array = [1, 2, 3, 4, 5]
 print "Please enter a number: "
 	def number(n)
+		if array.include(3)?
+			print "The number, " { n } "is contained within the first 5 numbers."
+		elsif array.include(9)?
+			print "The number, " { n } "does not appear within the first 5 numbers."
+		end
 	end
-print "The number, " m "is contained within the first 5 numbers."
-print "The number, " m "does not appear within the first 5 numbers."
+
+number(gets.to_i)
