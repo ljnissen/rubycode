f1d18ff4597271c3d480f38347bbc1a03a092c68
LJ Nissen <lj.nissen@gmail.com>
2017-07-31 10:45:55 +0200

    Understanding how to add numbers to array

---
+++ 007/file_02.rb
@@ -0,0 +1,26 @@
+
+
+print "Please enter an integer: "
+
+def array_setup(element)
+  array = Array.new(2)
+
+	(1..5).each do |element|
+		array << element
+	end
+	print array
+end
+array_setup(gets.to_i)
+
+
+
+def include_in_array
+	if array.include? 3
+		puts "The number 3 is included in the array."
+	elsif array.include? 9
+		puts "The number 9 is included in the array"
+	end
+end
+
+#include_in_array(gets.to_i)
+		
