07eb710eeb5184e4e2b6b052db8e09f6ca98a57a
LJ Nissen <lj.nissen@gmail.com>
2017-08-01 12:16:50 +0200

    Work on appending numbers to array

---
+++ 007/file_03.rb
@@ -0,0 +1,15 @@
+	def enter(number)
+		new_array = Array.new(0)
+		5.times do |number|
+			puts "Please enter a number: "	
+			new_array << number
+		end
+		print new_array
+	end
+	enter(gets.to_i)
+	
+
+
+
+
+
