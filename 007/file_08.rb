ae1b8b92220a228f491287033c7bccc5ab8c1529
LJ Nissen <lj.nissen@gmail.com>
2017-08-03 12:06:28 +0200

    Learning more about ruby loops

--- 007/file_08.rb
+++ 007/file_08.rb
@@ -1,10 +1,10 @@
-print "Enter the size of the array: "
-	n = (gets.to_i - 1)
-puts "Enter the array elements (one number for each new line): " + "\n"
-new_array = Array.new(n)
+#print "Please enter the size of the array: "
+#	n = (gets.to_i - 1)
+puts "Please enter the array elements (one number for each new line): " + "\n"
+	new_array = Array.new(5)
 
-for i in (0..n)
-  new_array[i]=gets.to_i
+for i in (0..4)
+  new_array[i] = gets.to_i
 end
 
 print new_array
