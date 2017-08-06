5454c3239ec995458eccde571ba723a1811ca93d
LJ Nissen <lj.nissen@gmail.com>
2017-08-06 17:09:37 +0200

    Added factorial code again

---
+++ 007/file_12.rb
@@ -0,0 +1,10 @@
+print "Please enter a number: "
+
+def factorial(number)
+	sum = 1
+	(1..number).each do |number|
+		sum = sum * number
+	end
+	sum
+end
+print "The factorial is: ", factorial(gets.to_i) 
