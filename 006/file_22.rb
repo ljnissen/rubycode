d319b9466384fa23d192d30f99f082c0c7349c7b
LJ Nissen <lj.nissen@gmail.com>
2017-08-08 09:39:46 +0200

    Repeat factorial code to learn

---
+++ 006/file_22.rb
@@ -0,0 +1,11 @@
+print "Please enter your number: "
+
+def factorial(number)
+	total = 1
+	(1..number).each do |number|
+		total = total * number
+	end
+	total
+end
+
+print "The factorial is: ", factorial(gets.to_i)
