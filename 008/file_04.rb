165857fd106b8da9deb627799f2220785fee8929
LJ Nissen <lj.nissen@gmail.com>
2017-08-17 11:51:48 +0200

    Understand some more football

--- 008/file_04.rb
+++ 008/file_04.rb
@@ -1,12 +1,12 @@
 print "Please enter the final score: "
-	final_score = gets.to_i
+	final_score = gets.to_i - 1
 	touchdowns = final_score / 7
 	push_ups = 0
 	
-	(0..touchdowns).each do |touchdowns|
-
-		push_ups = push_ups + touchdowns.to_i
-		push_ups = push_ups * 7
-
+	(0..touchdowns).each do |i|
+		i = 7
+		i <= final_score
+		i = i + 7
+		push_ups = push_ups + i
 	end
 print "Number of push-ups: ", push_ups 
