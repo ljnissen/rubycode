c829ab201da0d6e985aceecc2bf18c50c702c9a4
LJ Nissen <lj.nissen@gmail.com>
2017-08-20 16:26:32 +0200

    Not understanding football

---
+++ 008/file_07.rb
@@ -0,0 +1,11 @@
+print "Please enter the final score: "
+	final_score = gets.to_i
+	touchdowns = final_score / 7
+	push_ups = 0
+
+	(1..touchdowns).each do |i|
+		i = 7
+		i = i + 7
+		push_ups = push_ups + i
+	end
+print "The number of push-ups is: ", + push_ups
