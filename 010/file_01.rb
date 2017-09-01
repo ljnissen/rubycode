0a90207d4a5b37cce77f912d005609b50a144f84
LJ Nissen <lj.nissen@gmail.com>
2017-09-01 18:56:35 +0200

    Understanding problem 'shortest route in Alabama'

---
+++ 010/file_01.rb
@@ -0,0 +1,16 @@
+print "Please enter city #1: "
+ city_1 = gets
+print "Please enter city #2: "
+ city_2 = gets
+
+var Alabaster.to_s
+var Birmingham.to_s
+var Montgomery.to_s
+var Huntsville.to_s
+var Tuscaloosa.to_s
+var Demopolis.to_s
+var Mobile.to_s
+
+
+def shortest
+end
