794c8895e53a014d5bcbb3a6406e40b2fb0f5243
LJ Nissen <lj.nissen@gmail.com>
2017-09-07 20:24:48 +0200

    Check JS solution of Dijkstra's algorithm

--- 010/file_03.rb
+++ 010/file_03.rb
@@ -3,3 +3,14 @@ print "Please enter city #1: "
 print "Please enter city #2: "
 	city_2 = gets
 
+raw_data = ['Alabaster - Birmingham 24 miles', 
+			'Alabaster - Montgomery 71 miles',
+			'Birmingham - Huntsville 103 miles',
+			'Birmingham - Tuscaloosa 59 miles',
+			'Demopolis - Mobile 141 miles',
+			'Demopolis - Montgomery 101 miles',
+			'Demopolis - Tuscaloosa 65 miles',
+			'Mobile - Montgomery 169 miles',
+			'Montgomery - Tuscaloosa 134 miles']
+
+
