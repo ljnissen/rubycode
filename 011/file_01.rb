e175931c8de8b6249d6dfd699cc9bfa6157d009b
LJ Nissen <lj.nissen@gmail.com>
2017-09-19 18:17:06 +0200

    DRY calculator

--- 011/file_01.rb
+++ 011/file_01.rb
@@ -1,23 +1,23 @@
 def get_int_values
 	[gets, gets].map{ |s| s.chomp.to_i }
 end
-print "Would you like to [add], [multiply], [divide] or [subtract]? "
+print "Would you like to [a]dd, [m]ultiply, [d]ivide or [s]ubtract? "
 response = gets.chomp
 
-case response.downcase
-	when 'add'
+case response[0].downcase
+	when 'a'
 		puts "Which numbers would you like to add?"
 		operator = :+
 
-	when 'multiply'
+	when 'm'
 		puts "Which numbers would you like to multiply?"
 		operator = :*
 
-	when 'divide'
+	when 'd'
 		puts "Which numbers would you like to divide?"
 		operator = :/
 
-	when 'subtract'
+	when 's'
 		puts "Which numbers would you like to subtract?"
 		operator = :-
 end
