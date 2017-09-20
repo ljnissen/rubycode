4c073176e8ee1ece8cc66a618391d361281d1a5d
LJ Nissen <lj.nissen@gmail.com>
2017-09-20 15:38:09 +0200

    Try to repeat calculator code

--- 011/file_02.rb
+++ 011/file_02.rb
@@ -1,2 +1,16 @@
 print "Would you like to [a]dd, [s]ubtract, [m]ultiply or [d]ivide? "
 	response = gets.chomp
+
+case response[0].downcase
+	when "a"
+	operator = :+
+
+	when "s"
+	operator = :-
+
+	when "m"
+	operator = :*
+
+	when "d"
+	operator = :-
+end
